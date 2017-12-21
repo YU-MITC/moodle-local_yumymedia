107d1600a1e93c2d32d71c82865366d2fdf6c56f
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2017-12-21 14:49:44 +0900

    Add files via upload

--- check_conversion.php
+++ check_conversion.php
@@ -18,7 +18,7 @@
  * Conversion check script in "My Media".
  *
  * @package    local_yumymedia
- * @copyright  (C) 2016-2017 Yamaguchi University <info-cc@ml.cc.yamaguchi-u.ac.jp>
+ * @copyright  (C) 2016-2017 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 

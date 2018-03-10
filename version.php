8b05f7e2fec8660ed40e97f63211c4d1058b3523
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2018-03-10 20:08:50 +0900

    Add files via upload

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018022000;
+$plugin->version = 2018031000;
 $plugin->component = 'local_yumymedia';
-$plugin->release = 'YU Kaltura Media Gallery 1.1.3';
+$plugin->release = 'YU Kaltura Media Gallery 1.1.4';
 $plugin->requires = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2018022000
+    'local_yukaltura' => 2018031000
 );

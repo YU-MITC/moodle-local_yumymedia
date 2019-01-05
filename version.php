779ce1e53a65d958edccbb9b4d3a5d24ae318146
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2019-01-06 03:07:53 +0900

    Add files via upload

--- version.php
+++ version.php
@@ -18,17 +18,17 @@
  * YU Kaltura Media Gallery version file
  *
  * @package    local_yumymedia
- * @copyright  (C) 2016-2018 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @copyright  (C) 2016-2019 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018120300;
+$plugin->version = 2019010700;
 $plugin->component = 'local_yumymedia';
-$plugin->release = 'YU Kaltura Media Gallery 1.2.0';
+$plugin->release = 'YU Kaltura Media Gallery 1.2.1';
 $plugin->requires = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2018120300
+    'local_yukaltura' => 20190107
 );

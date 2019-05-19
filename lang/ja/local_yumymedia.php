9ffc794f1fa77cc83b09ec7b4a4550f202b13c49
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2019-05-20 02:54:12 +0900

    Add files via upload

--- lang/ja/local_yumymedia.php
+++ lang/ja/local_yumymedia.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'My Media';
+$string['pluginname'] = 'マイメディア';
 
 // Capability strings.
 $string['yumymedia:view'] = 'マイメディアページを表示する。';

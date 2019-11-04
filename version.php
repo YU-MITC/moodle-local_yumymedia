0da7045a5d4dfa9fc8733791137ebfef6ac8ff1b
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2019-11-04 21:51:18 +0900

    Update for GDPR

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yumymedia';
-$plugin->version = 2019091200;
-$plugin->release = '1.3.2';
+$plugin->version = 2019110300;
+$plugin->release = '1.3.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015051100;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2019083000
+    'local_yukaltura' => 2019110300
 );

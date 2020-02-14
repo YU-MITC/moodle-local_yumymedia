b3c47c4221cc1dbac4486cffa8f25acd48e4d96c
YU-MITC <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2020-02-15 01:34:06 +0900

    Fix search keyword handling

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yumymedia';
-$plugin->version = 2019121500;
-$plugin->release = '1.4.0R2';
+$plugin->version = 2020021400;
+$plugin->release = '1.4.0R3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015051100;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2019111400
+    'local_yukaltura' => 2020021400
 );

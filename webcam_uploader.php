5f8fd06af166c69f93169abfbacc0f85cac526b5
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2022-03-15 13:56:52 +0900

    Update

--- webcam_uploader.php
+++ webcam_uploader.php
@@ -18,7 +18,7 @@
  * Webcam recording and uploader script in YU Kaltura My Media Gallery.
  *
  * @package    local_yumymedia
- * @copyright  (C) 2016-2021 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @copyright  (C) 2016-2022 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 

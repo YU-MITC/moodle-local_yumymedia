df73d06252adab7225d978652d0e78ac2c26f81c
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2023-01-16 21:40:39 +0900

    Update

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yumymedia';
-$plugin->version = 2023011000;
-$plugin->release = '2.1.0';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2020061500;
-$plugin->supported = [40, 41];
+$plugin->version = 2023011600;
+$plugin->release = '2.1.0BETA1';
+$plugin->maturity = MATURITY_BETA;
+$plugin->requires = 2022041900;
+$plugin->supported = [400, 401];
 $plugin->dependencies = array(
-    'local_yukaltura' => 2022123000
+    'local_yukaltura' => 2023011600
 );

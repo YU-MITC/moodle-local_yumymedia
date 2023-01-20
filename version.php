e6f9d03630a6ba4ce9a8ef1222044b7228d628dd
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2023-01-20 21:36:39 +0900

    Update

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yumymedia';
-$plugin->version = 2023012000;
-$plugin->release = '2.1.0';
+$plugin->version = 2023012001;
+$plugin->release = '2.1.0R2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900;
 $plugin->supported = [400, 401];
 $plugin->dependencies = array(
-    'local_yukaltura' => 2023012000
+    'local_yukaltura' => 2023012001
 );

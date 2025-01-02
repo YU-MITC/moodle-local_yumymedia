ecba20fda2a9ff68de1632ccb3775f4a1d06f5c2
Center for Information Infrastructure <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2025-01-02 17:49:10 +0900

    Update version.php

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yumymedia';
-$plugin->version = 2023012001;
-$plugin->release = '2.1.0R2';
+$plugin->version = 2025010200;
+$plugin->release = '3.0.0';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2022041900;
-$plugin->supported = [400, 401];
+$plugin->requires = 2023100900;
+$plugin->supported = [403, 405];
 $plugin->dependencies = array(
-    'local_yukaltura' => 2023012001
+    'local_yukaltura' => 2025010200
 );
